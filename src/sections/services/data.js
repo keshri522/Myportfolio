f2bb3ebb08c18a7fb0e0d81080fb34959ae4f84c
keshri522 <rkeshri522@gmail.com>
2023-08-30 23:28:22 +0530

    added data file in serverice componets

--- src/sections/services/data.js
+++ src/sections/services/data.js
@@ -1,23 +1,30 @@
-import {SiAdobexd} from 'react-icons/si'
-import {RiReactjsLine} from 'react-icons/ri'
-import {FaServer} from 'react-icons/fa'
-import {AiFillAppstore} from 'react-icons/ai'
-
+import { SiAdobexd } from "react-icons/si";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaServer } from "react-icons/fa";
+import { AiFillAppstore } from "react-icons/ai";
 
 const data = [
-    {
-        id: 1, icon: <SiAdobexd/>, title: 'UI/UX Design', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
-    },
-    {
-        id: 2, icon: <RiReactjsLine/>, title: 'Frontend Development', desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop."
-    },
-    {
-        id: 3, icon: <FaServer/>, title: 'Backend Development', desc: "The security of your business/product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks."
-    },
-    {
-        id: 4, icon: <AiFillAppstore/>, title: 'App Development', desc: 'Unlike other developers in the market, I will building an app that runs on both IOS and Android devices without any extra cost to you.'
-    }
-]
+  // {
+  //     id: 1, icon: <SiAdobexd/>, title: 'UI/UX Design', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
+  // },
+  {
+    id: 1,
+    icon: <RiReactjsLine />,
+    title: "Frontend Development",
+    desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop.",
+  },
+  {
+    id: 2,
+    icon: <FaServer />,
+    title: "Backend Development",
+    desc: "The security of your business/product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks.",
+  },
+  {
+    id: 3,
+    icon: <AiFillAppstore />,
+    title: "Full Stack Development",
+    desc: "A MERN Stack Developer specializes in building web applications using a combination of technologies: MongoDB, Express.js, React, and Node.js. This tech stack allows developers to create dynamic and efficient applications that cover both frontend and backend development aspects. MERN Stack Developers are skilled in various programming languages and tools, enabling them to craft end-to-end solutions that cater to user experience, functionality, and performance.",
+  },
+];
 
-
-export default data
+export default data;
