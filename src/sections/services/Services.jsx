14cc40c134ebb8935d4e9a825b3a4d35b92e1548
keshri522 <rkeshri522@gmail.com>
2023-08-30 23:22:16 +0530

    added Service jsx

--- src/sections/services/Services.jsx
+++ src/sections/services/Services.jsx
@@ -1,9 +1,25 @@
-import './services.css'
+import Card from "../../components/Card";
+import data from "./data";
+import "./services.css";
 
 const Services = () => {
   return (
-    <section id="services"><h2>Services</h2></section>
-  )
-}
+    <section id="services">
+      <h2>My Services</h2>
+      <p>I give you the best in all the services below</p>
+      <div className="container services__container">
+        {data.map((item) => (
+          <Card key={item.id} className="service light">
+            <div className="service__icon">{item.icon}</div>
+            <div className="service__details">
+              <h4>{item.title}</h4>
+              <p>{item.desc}</p>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Services
+export default Services;
