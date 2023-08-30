48f304e8c9b4e8b61059b2fcf1dd64d76df922f4
keshri522 <rkeshri522@gmail.com>
2023-08-30 22:26:30 +0530

    added navbar and css of the navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import './navbar.css'
+import Logo from "../../assets/logo.jpg";
+import data from "./data";
+import { IoIosColorPalette } from "react-icons/io";
+import "./navbar.css";
 
 const Navbar = () => {
+  // this is the navbar which contain all the navbars items in the data which improts
   return (
-    <nav><h2>Navbar</h2></nav>
-  )
-}
+    <nav>
+      <div className="container nav__container">
+        <a href="index.html" className="nav__logo">
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className="nav__menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id="theme__icon">
+          <IoIosColorPalette />
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
