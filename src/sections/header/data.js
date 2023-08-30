50dfefa21470f82d64fe649263b6b2eae4fe6422
keshri522 <rkeshri522@gmail.com>
2023-08-30 22:59:21 +0530

    added icons and link of icons page

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,13 +1,19 @@
-import {AiOutlineInstagram} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
-import {AiFillGithub} from 'react-icons/ai'
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
 const data = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
-]
+  {
+    id: 2,
+    link: "https://www.facebook.com/rahul.keshri.5011",
+    icon: <AiOutlineFacebook />,
+  },
+  {
+    id: 3,
+    link: "https://www.linkedin.com/in/rahul-keshri-814bb8221/",
+    icon: <AiOutlineLinkedin />,
+  },
+  { id: 4, link: "https://github.com/keshri522", icon: <AiFillGithub /> },
+];
 
-export default data
+export default data;
