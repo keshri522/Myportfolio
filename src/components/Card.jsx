c11be060be5c54cd804abee23cb2ee2259670f85
keshri522 <rkeshri522@gmail.com>
2023-08-30 23:07:00 +0530

    added Cart resuable components

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import "./card.css";
+
+const Card = ({ children, className, onClick }) => {
+  return (
+    <article className={`card ${className}`} onClick={onClick}>
+      {children}
+    </article>
+  );
+};
+
+export default Card;
