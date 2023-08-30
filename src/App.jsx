8a2ffa084a4563916b1a946841f8cb15c75aed02
keshri522 <rkeshri522@gmail.com>
2023-08-30 22:26:47 +0530

    add navbar and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import Navbar from './sections/navbar/Navbar';
-import Header from './sections/header/Header';
-import About from './sections/about/About';
-import Services from './sections/services/Services';
-import Portfolio from './sections/portfolio/Portfolio';
-import Testimonials from './sections/testimonials/Testimonials';
-import FAQs from './sections/faqs/FAQs';
-import Contact from './sections/contact/Contact';
-import Footer from './sections/footer/Footer';
-import FloatingNav from './sections/floating-nav/FloatingNav';
-
+import Navbar from "./sections/navbar/Navbar";
+import Header from "./sections/header/Header";
+import About from "./sections/about/About";
+import Services from "./sections/services/Services";
+import Portfolio from "./sections/portfolio/Portfolio";
+import Testimonials from "./sections/testimonials/Testimonials";
+import FAQs from "./sections/faqs/FAQs";
+import Contact from "./sections/contact/Contact";
+import Footer from "./sections/footer/Footer";
+import FloatingNav from "./sections/floating-nav/FloatingNav";
+// this is main which will handle all the componets globally
 const App = () => {
   return (
     <main>
-        <Navbar/>
-        <Header/>
-        <About/>
-        <Services/>
-        <Portfolio/>
-        <Testimonials/>
-        <FAQs/>
-        <Contact/>
-        <Footer/>
-        <FloatingNav/>
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <FAQs />
+      <Contact />
+      <Footer />
+      <FloatingNav />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/sections/navbar/data.js
@@ -0,0 +1,10 @@
+
+const data = [
+    {id: 1, link: '#', title: 'Home'},
+    {id: 2, link: '#about', title: 'About'},
+    {id: 3, link: '#services', title: 'Services'},
+    {id: 4, link: '#portfolio', title: 'Portfolio'},
+    {id: 5, link: '#contact', title: 'Contact'}
+    ]
+
+    export default data;
