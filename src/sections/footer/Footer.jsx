b73c748dfe767aff01891d0418ce0bc971c2e52e
keshri522 <rkeshri522@gmail.com>
2023-08-31 15:22:40 +0530

    added footer components

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,8 +1,25 @@
+import {links, socials} from './data'
 import './footer.css'
 
 const Footer = () => {
   return (
-    <footer><h2>Footer</h2></footer>
+    <footer>
+      <div className="container footer__container">
+        <ul className='nav__menu'>
+          {
+            links.map(fLink => <li key={fLink.id}><a href={fLink.link}>{fLink.title}</a></li>)
+          }
+        </ul>
+        <div className="footer__socials">
+          {
+            socials.map(social => <a key={social.id} href={social.link} target="_blank" rel="noopener noreferrer">{social.icon}</a>)
+          }
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <small>2022 EGATOR TUTORIALS &copy; All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
 
