db17f467b4b1300cf59ccdac6526c8b5e44a5458
keshri522 <rkeshri522@gmail.com>
2023-08-31 14:59:11 +0530

    added contact page

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,8 +1,19 @@
+import contacts from './data'
 import './contact.css'
 
 const Contact = () => {
   return (
-    <section id="contact"><h2>Contacts</h2></section>
+    <section id="contact">
+      <h2>Get In Touch</h2>
+      <p>
+        Shoot me a message via any of the links below!
+      </p>
+      <div className="container contact__container">
+        {
+          contacts.map(contact => <a key={contact.id} href={contact.link} target="_blank" rel="noopener noreferrer">{contact.icon}</a>)
+        }
+      </div>
+    </section>
   )
 }
 
