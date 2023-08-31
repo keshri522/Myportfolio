b9256d665dbfc30b236106e9ed7832de87490ea7
keshri522 <rkeshri522@gmail.com>
2023-08-31 15:21:09 +0530

    added contact us page and link of whatsapp

---
+++ src/sections/contact/data.js
@@ -0,0 +1,15 @@
+import { HiOutlineMail } from "react-icons/hi";
+import { RiMessengerLine } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
+
+const data = [
+  { id: 1, icon: <HiOutlineMail />, link: "mailto: rkeshri522@gmail.com" },
+  {
+    id: 2,
+    icon: <RiMessengerLine />,
+    link: "https://www.linkedin.com/in/rahul-keshri-814bb8221/",
+  },
+  { id: 3, icon: <FaWhatsapp />, link: "https://wa.me/+917631701413" },
+];
+
+export default data;
