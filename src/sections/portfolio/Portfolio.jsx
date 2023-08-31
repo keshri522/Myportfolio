54683d1f41066070e0cd19d8097115b127395c80
keshri522 <rkeshri522@gmail.com>
2023-08-31 12:18:15 +0530

    added portfolio componets

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,9 +1,43 @@
-import './portfolio.css'
+import "./portfolio.css";
+import Projects from "./Projects";
+import ProjectsCategories from "./ProjectsCategories";
+import data from "./data";
+import { useState } from "react";
 
 const Portfolio = () => {
-  return (
-    <section id="portfolio"><h2>Portfolio</h2></section>
-  )
-}
+  const [projects, setProjects] = useState(data);
 
-export default Portfolio
+  const categories = data.map((item) => item.category);
+  const uniqueCategories = ["all", ...new Set(categories)];
+
+  const filterProjectsHandler = (category) => {
+    if (category === "all") {
+      setProjects(data);
+      return;
+    }
+
+    const filterProjects = data.filter(
+      (project) => project.category === category
+    );
+    setProjects(filterProjects);
+  };
+
+  return (
+    <section id="portfolio">
+      <h2>Recent Projects</h2>
+      <p>
+        Check out some of the projects I recently worked on for my clients. Use
+        the buttons to toggle the different categories.
+      </p>
+      <div className="container portfolio__container">
+        <ProjectsCategories
+          categories={uniqueCategories}
+          onFilterProjects={filterProjectsHandler}
+        />
+        <Projects projects={projects} />
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
