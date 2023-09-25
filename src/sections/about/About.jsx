04776a25d2a996d46c071e1102b4198fd127c10b
keshri522 <rkeshri522@gmail.com>
2023-09-25 13:21:32 +0530

    added gh pages

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,6 +1,6 @@
 import AboutImage from "../../assets/aboutpic.jpg";
 
-import CV from "../../assets/keshri.pdf";
+import CV from "../../assets/keshri1.pdf";
 import { HiDownload } from "react-icons/hi";
 import data from "./data"; // this is the data importing
 import Card from "../../components/Card";
