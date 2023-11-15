4da6c501cae779f5f6faa2448788f49674a6c950
keshri522 <rkeshri522@gmail.com>
2023-11-15 13:40:41 +0530

    added progresss bar and animation on navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,14 +1,59 @@
-import Logo from "../../assets/logo.jpg";
-import { useState } from "react";
-import FeedbackModal from "../../Modal/modals";
-import data from "./data";
-import { IoIosColorPalette } from "react-icons/io";
-import "./navbar.css";
-import Mypic from "../../assets/aboutpic.jpg";
+// import Logo from "../../assets/logo.jpg";
+// import { useState } from "react";
+// import FeedbackModal from "../../Modal/modals";
+// import data from "./data";
+// import { IoIosColorPalette } from "react-icons/io";
+// import "./navbar.css";
+// import Mypic from "../../assets/aboutpic.jpg";
+// import { faMessage } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// const Navbar = () => {
+//   const [showModal, setShowModal] = useState(false);
+
+//   const handleShowModal = () => {
+//     setShowModal(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setShowModal(false);
+//   };
+//   // this is the navbar which contain all the navbars items in the data which improts
+//   return (
+//     <nav>
+//       <div className="container nav__container">
+//         <label style={{ cursor: "pointer", color: "white" }}>
+//           <input
+//             type="button"
+//             style={{ display: "none" }}
+//             onClick={handleShowModal}
+//           />
+//           <FontAwesomeIcon icon={faMessage}></FontAwesomeIcon>
+//         </label>
+
+//         <FeedbackModal showModal={showModal} handleClose={handleCloseModal} />
+//         <ul className="nav__menu">
+//           {data.map((item) => (
+//             <li key={item.id}>
+//               <a href={item.link}>{item.title}</a>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+import React, { useState, useEffect } from "react";
 import { faMessage } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FeedbackModal from "../../Modal/modals";
+import data from "./data";
+import "./navbar.css";
+
 const Navbar = () => {
   const [showModal, setShowModal] = useState(false);
+  const [scrollProgress, setScrollProgress] = useState(0);
 
   const handleShowModal = () => {
     setShowModal(true);
@@ -17,13 +62,25 @@ const Navbar = () => {
   const handleCloseModal = () => {
     setShowModal(false);
   };
-  // this is the navbar which contain all the navbars items in the data which improts
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const totalHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const progress = (window.scrollY / totalHeight) * 100;
+      setScrollProgress(progress);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <nav>
       <div className="container nav__container">
-        {/* <a href="index.html" className="nav__logo">
-          <img src={Mypic} alt="Logo" />
-        </a> */}
         <label style={{ cursor: "pointer", color: "white" }}>
           <input
             type="button"
@@ -34,16 +91,22 @@ const Navbar = () => {
         </label>
 
         <FeedbackModal showModal={showModal} handleClose={handleCloseModal} />
+
+        {/* Progress bar at the top */}
+        <div
+          className="scroll-progress"
+          style={{ width: `${scrollProgress}%` }}
+        />
+
         <ul className="nav__menu">
           {data.map((item) => (
             <li key={item.id}>
-              <a href={item.link}>{item.title}</a>
+              <a className="animation" href={item.link}>
+                {item.title}
+              </a>
             </li>
           ))}
         </ul>
-        {/* <button id="theme__icon">
-          <IoIosColorPalette />
-        </button> */}
       </div>
     </nav>
   );
--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -3,8 +3,8 @@ const data = [
   { id: 2, link: "#about", title: "About" },
   { id: 3, link: "#services", title: "Services" },
   { id: 4, link: "#portfolio", title: "Projects" },
-  { id: 5, link: "#contact", title: "Contact" },
-  { id: 6, link: "#skill", title: "Skill" },
+  { id: 5, link: "#skill", title: "Skill" },
+  { id: 6, link: "#contact", title: "Contact" },
 ];
 
 export default data;
